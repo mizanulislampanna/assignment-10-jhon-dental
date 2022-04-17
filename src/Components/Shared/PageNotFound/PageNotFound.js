23a52741065a2f5ce0aa211df65d8d9679b4c91a
Mizanul Islam Panna <mizanulislampanna>
2022-04-17 17:56:11 +0600

    header added

---
+++ src/Components/Shared/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <h3>The page you are looking for is not Avilable</h3>
+    </div>
+  );
+};
+
+export default PageNotFound;
